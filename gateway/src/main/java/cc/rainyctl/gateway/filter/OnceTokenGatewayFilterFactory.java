7e0cdfb8e303e514b4e845f50d0993cdeecfc35b
rainyctl <rainyctl@gmail.com>
2026-01-10 14:38:38 -0800

    feat: update gateway filters and configuration

--- gateway/src/main/java/cc/rainyctl/gateway/filter/OnceTokenGatewayFilterFactory.java
+++ gateway/src/main/java/cc/rainyctl/gateway/filter/OnceTokenGatewayFilterFactory.java
@@ -1,4 +1,39 @@
 package cc.rainyctl.gateway.filter;
 
-public class OnceTokenGatewayFilterFactory {
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.factory.AbstractNameValueGatewayFilterFactory;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.util.UUID;
+
+@Component
+public class OnceTokenGatewayFilterFactory extends AbstractNameValueGatewayFilterFactory {
+
+    @Override
+    public GatewayFilter apply(NameValueConfig config) {
+        return new GatewayFilter() {
+            @Override
+            public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+                // add a token to the response header
+                return chain.filter(exchange).then(Mono.fromRunnable(() -> {
+                    ServerHttpResponse response = exchange.getResponse();
+                    String tokenName = config.getName();
+                    String value = config.getValue();
+                    String token = null;
+                    if ("uuid".equals(value)) {
+                        token = UUID.randomUUID().toString();
+                    } else if ("jwt".equals(value)) {
+                        token = "aaa.bbb.ccc"; // close enough :)
+                    } else {
+                        token = "random-token";
+                    }
+                    exchange.getResponse().getHeaders().add(tokenName, token);
+                }));
+            }
+        };
+    }
 }
--- gateway/src/main/java/cc/rainyctl/gateway/filter/ResponseTimeGlobalFilter.java
+++ gateway/src/main/java/cc/rainyctl/gateway/filter/ResponseTimeGlobalFilter.java
@@ -1,4 +1,34 @@
 package cc.rainyctl.gateway.filter;
 
-public class ResponseTimeGlobalFilter {
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+// no registry required, apply automatically
+@Slf4j
+@Component
+public class ResponseTimeGlobalFilter implements GlobalFilter, Ordered {
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        ServerHttpRequest request = exchange.getRequest();
+        ServerHttpResponse response = exchange.getResponse();
+
+        String uri = request.getURI().toString();
+        long start = System.currentTimeMillis();
+        log.info("Request {} received at {}", uri, start);
+
+        return chain.filter(exchange)
+                .doFinally(signalType -> log.info("Request {} took {} ms", uri, System.currentTimeMillis() - start));
+    }
+
+    @Override
+    public int getOrder() {
+        return 0;
+    }
 }
